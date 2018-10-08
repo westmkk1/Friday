c8f162e426cd485e08786a5cbd0da1e2b6120622
westmkk1 <36356646+westmkk1@users.noreply.github.com>
2018-10-08 14:54:25 -0700

    Added functions to BinaryDevice allowin you to disconnect the device and get the device name

--- Server/BinaryDevice.py
+++ Server/BinaryDevice.py
@@ -1,23 +1,51 @@
 import DeviceConnection
+import time
+import ServerInfo
 
 class BinaryDevice:
-
+	
+	#Provide a connection to the device
     def __init__(self, deviceConnection):
         self.deviceName = deviceConnection.getDeviceName()
         self.deviceType = deviceConnection.getDeviceType()
-
         self.connection = deviceConnection
+		
+		self.lastActiveTime = time.time()
 
     #Renew the connection to the device so we dont use the same socket all day
     def renewConnection(self):
         self.connection.reconnectToDevice(self)
+		
+		#Say the last time the connection was active
+		self.lastActiveTime = time.time()
 
+		
     #Send an off signal to the device
     def switchOff(self):
+		#Check if the connection need to be renewed
+		if (time.time() - self.lastActiveTime) >= ServerInfo.SOCKET_RENEW_TIME:
+			self.renewConnection()
+			
         self.connection.sendData("DOff")
 
+	
+	#Send an On signal to the device
     def switchOn(self):
+		#Check if the connection need to be renewed
+		if (time.time() - self.lastActiveTime) >= ServerInfo.SOCKET_RENEW_TIME:
+			self.renewConnection()
+			
         self.connection.sendData("DeOn")
+		
+	
+	def getDeviceName(self):
+		return self.connection.getDeviceName();
+	
+	#Terminate the connection between the server and the device
+	def disconnectDevice(self):
+		self.connection.closeConnection()
+		self.connection = None
+		
         
-
+	
     
--- Server/ServerInfo.py
+++ Server/ServerInfo.py
@@ -4,6 +4,9 @@ SECURITY_CODE = "j4ba80!*"
 SECURITY_CODE_BYTES = 8
 DEVICE_NAME_LENGTH_MAX = 25
 
+#How often the sockets of a connection should be replaced
+SOCKET_RENEW_TIME = 5 #Every x seconds renew the socket
+
 
 #Packet sizes assosciated with device type
 Device_Packet_Sizes = {
